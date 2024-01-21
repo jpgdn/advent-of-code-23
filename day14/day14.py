42646d305de2c28c0ab34fec6c07deff2728b4fa
Jonas Gårding <jonas.garding@elekta.com>
2024-01-21 22:59:46 +0100

    Day 14 cleanup

--- day14/day14.py
+++ day14/day14.py
@@ -14,13 +14,14 @@ def score_grid(grid):
 
 def tilt_grid(grid):
     pos = [0] * len(grid[0])    # First free position for each column
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 'O':
+    for i, row in enumerate(grid):
+        for j, c in enumerate(row):
+            if c == 'O':
+                # Move O to the first free position (could be its present position)
                 grid[i][j] = '.'
                 grid[pos[j]][j] = 'O'
                 pos[j] += 1
-            elif grid[i][j] == '#':
+            elif c == '#':
                 pos[j] = i+1
     return grid
 
@@ -38,25 +39,25 @@ def part2(fn,spins=20):
 
     grid_cache = dict()
     res_list = []
-    start = 0
-    cycle = 1
     for i in range(spins):
         for d in range(4):
             grid = tilt_grid(grid)
             grid = [list(row)[::-1] for row in zip(*grid)]  # Rotate 90 degrees
         g = tuple(tuple(row) for row in grid)   # Hash key
         if g in grid_cache:
+            # We found a cycle
             cycle = i - grid_cache[g]
             start = grid_cache[g]
+            print(f'{start = }, {cycle = }')
+            ix = start + (spins - start - 1) % cycle
             break
         grid_cache[g] = i
         res_list.append(score_grid(grid))
+    else:   # We didn't find a cycle
+        ix = spins-1
     
-    print(f'{start = }, {cycle = }')
-    ix = start + (spins - start - 1) % cycle
     print(f'{ix = }')
-    res = res_list[ix]
-    return res
+    return res_list[ix]
 
 if __name__ == "__main__":
     fn = 'aoc23-14-input.txt'
